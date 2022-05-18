aad091c29ea9b4f28bd00754fa59360ab4038853
Hao Lin <68680028+uoa-hlin784@users.noreply.github.com>
2022-05-18 16:38:01 +1200

    Transferred datapath c files to cruise_control.h

--- src/cruise_control.h
+++ src/cruise_control.h
@@ -1,2 +1,22 @@
- 
+/* Created by Hao Lin and Chamith Nanayakkara
+    this file implements the datapath component of the cruise control
+    for example:
+    - Speed
+    - CruiseSpeed
+    - CruiseState
+    - ThrottleCmd
+    should all have an corresponding variable here. Inc intermediate signals such as:
+    - isAccel
+    - isBrake
+*/
+
+/* Note: */
+//The type 'string' is defined as 'char*' in Esterel, hence passing a string by referenc is 'char**'
+//Aviod naming a function 'recv'
+
+/* Libarires */
+// #include<stdio.h>
+// #include<stdlib.h>
+
+/* PI Controller */
 float regulateThrottle(int isGoingOn, float cruiseSpeed, float vehicleSpeed);
